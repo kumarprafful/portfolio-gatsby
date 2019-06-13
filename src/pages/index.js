080f10866e91e2d0d879750fe45131fc58fa4a77
Prafful Kumar <kumarprafful.54@gmail.com>
2019-06-13 12:20:35 +0530

    first

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,13 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
-
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Prafful Kumar</h1>
+      <h2>I'm a full stack developer based in New Delhi.</h2>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  )
+}
 
 export default IndexPage
